039699c9067e46dba2d80d79cc32d56a51117f8c
Kelsey Jackson <kelseythej@gmail.com>
2022-08-31 11:38:46 -0500

    initial commit

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <font-awesome-icon icon="fas fa-user-secret" size="4x" />
+    <font-awesome-icon icon="fass fa-user-secret" size="4x" />
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+import { defineNuxtConfig } from "nuxt";
+
+// Modify the `nuxt.config.ts` file by adding to the `export default defineNuxtConfig()`
+export default defineNuxtConfig({
+  css: ["@fortawesome/fontawesome-svg-core/styles.css"],
+});
